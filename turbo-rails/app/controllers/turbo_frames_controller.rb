0f5310285b946171afa92dfe4ecd75b79e7c021a
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2022-05-07 16:33:18 +1000

    add simple turbo frame examples

---
+++ turbo-rails/app/controllers/turbo_frames_controller.rb
@@ -0,0 +1,5 @@
+class TurboFramesController < ApplicationController
+  def index; end
+
+  def new; end
+end
--- turbo-rails/config/routes.rb
+++ turbo-rails/config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'turbo_frames#index'
+
+  resources :turbo_frames, only: %i[index new]
+
   resources :rooms do
     resources :messages
   end
