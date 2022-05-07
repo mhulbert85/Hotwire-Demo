e94665952ed40829824eba5f747e4fc98f276af3
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2022-05-07 15:51:35 +1000

    add rubocop & .toolsversions

--- turbo-rails/app/controllers/turbo_frames_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class TurboFramesController < ApplicationController
-  def index; end
-
-  def new; end
-end
--- turbo-rails/config/routes.rb
+++ turbo-rails/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'turbo_frames/index'
-  get 'turbo_frames/new'
   resources :rooms do
     resources :messages
   end
