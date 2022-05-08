fb64ec3dc8674b5e6d4e0b986f69afb97d1d6b86
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2022-05-09 09:00:50 +1000

    add shared partials examples

---
+++ turbo-rails/app/controllers/turbo_frames/shared_partials_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class TurboFrames::SharedPartialsController < ApplicationController
+  # provides a reusable route that accepts a partial as a parameter
+  def new
+    render partial: params[:partial],
+           locals: params[:locals],
+           as: params[:as],
+           object: params[:object]
+  end
+
+  def index; end
+end
--- turbo-rails/config/routes.rb
+++ turbo-rails/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :turbo_frames do
     resources :nested_frames, only: %i[index new show]
+    resources :shared_partials, only: %i[index new]
     resources :simple_frames, only: %i[index new]
   end
 
