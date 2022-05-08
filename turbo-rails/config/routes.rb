fd278d3a8b1b6e7768b562c56e70bf712055bd45
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2022-05-08 11:12:14 +1000

    update nesting and add index page

--- turbo-rails/config/routes.rb
+++ turbo-rails/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'turbo_frames/nested_frames#index'
+  root 'pages#index'
 
   namespace :turbo_frames do
     resources :nested_frames, only: %i[index new show]
