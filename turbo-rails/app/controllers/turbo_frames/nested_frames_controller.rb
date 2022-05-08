722ef31af4fd86fbe2fbba314216bd405289239a
Mike Hulbert <mike.hulbert85@gmail.com>
2022-05-08 10:37:44 +1000

    Nested turbo frames (#3)
    * add simple turbo frame examples
    
    * create nested frames
    
    * refactor nested turbo frame examples
    
    * namespace turbo frames VCs
    
    * use path with random id

---
+++ turbo-rails/app/controllers/turbo_frames/nested_frames_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class TurboFrames::NestedFramesController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def show; end
+end
--- turbo-rails/config/routes.rb
+++ turbo-rails/config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'turbo_frames/nested_frames#index'
+
+  namespace :turbo_frames do
+    resources :nested_frames, only: %i[index new show]
+  end
+
   resources :rooms do
     resources :messages
   end
